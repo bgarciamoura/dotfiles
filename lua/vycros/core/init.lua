43f47d83a4252b8ba7b635d29d8e7b3e7fff21d1
Bruno Garcia Moura <bgarciamoura@gmail.com>
2024-02-27 19:01:09 -0300

    feat: Included telescope-themes
    This is a theme switcher extension for telescope that allows to preview an switch the color scheme instantly

--- lua/vycros/core/init.lua
+++ lua/vycros/core/init.lua
@@ -1,2 +1,2 @@
-require("vycros.core.keymaps")
 require("vycros.core.settings")
+require("vycros.core.keymaps")
--- lua/vycros/core/settings.lua
+++ lua/vycros/core/settings.lua
@@ -1,4 +1,11 @@
 local opt = vim.opt
 
 -- Remove the ~ char on empty lines
-opt.fillchars = { eob = "" }
+-- opt.fillchars = { eob = "" }
+
+-- Disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- Setting the configs for colorscheme
+vim.o.termguicolors = true
