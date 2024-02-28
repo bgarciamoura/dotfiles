81f745f7319121391ccb9d3612ea407ee13db251
Bruno Garcia Moura <bgarciamoura@gmail.com>
2024-02-27 22:06:54 -0300

    feat: Included arrow.nvim

---
+++ lua/vycros/plugins/arrow.lua
@@ -0,0 +1,30 @@
+return {
+  	"otavioschwanck/arrow.nvim",
+	config = function() 
+		require("arrow").setup({
+			show_icons = true,
+			save_path = function()
+				return vim.fn.stdpath("cache") .. "/arrow"
+			end,
+			mappings = {
+				edit = "e",
+				delete_mode = "d",
+				clear_all_items = "C",
+				toggle = "s", -- used as save if separate_save_and_remove is true
+				open_vertical = "v",
+				open_horizontal = "-",
+				quit = "q",
+				remove = "x", -- only used if separate_save_and_remove is true
+			},
+			window = {	
+				width = "auto",
+				height = "auto",
+				row = "auto",
+				col = "auto",
+				border = "double",
+			},
+			leader_key = ";",
+			index_keys = "123456789zxcbnmZXVBNM,afghjklAFGHJKLwrtyuiopWRTYUIOP",
+		})
+	end,
+}
