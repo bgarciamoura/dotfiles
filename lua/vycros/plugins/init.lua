6b6fb5cae0b8ac22c76d8a8cb17010afbf1e5ea0
Bruno Garcia Moura <bgarciamoura@gmail.com>
2024-03-01 18:13:24 -0300

    fix: Removed some infos

--- lua/vycros/plugins/init.lua
+++ lua/vycros/plugins/init.lua
@@ -5,7 +5,6 @@ return {
   { "tpope/vim-commentary" },
   { "windwp/nvim-ts-autotag" },
   { "LudoPinelli/comment-box.nvim" },
-  -- waka_1f52a544-4491-48fe-a6aa-b2afd32fb1e8
   { "wakatime/vim-wakatime", lazy = false },
 
   -- Themes
