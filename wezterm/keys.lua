115f619d12046e958f3702fd8ba73313a6ed524d
Bruno Garcia Moura <bgmoura@macbookairm1.local>
2024-06-20 15:31:01 -0300

    feat: Remapped the way i create new panels

--- wezterm/keys.lua
+++ wezterm/keys.lua
@@ -3,35 +3,37 @@ local wezterm = require 'wezterm'
 return {
 
   -- Abrir uma nova aba
-  {key="n", mods="SUPER", action=wezterm.action{SpawnTab="CurrentPaneDomain"}},
-  
+  { key = "n",          mods = "SUPER",       action = wezterm.action { SpawnTab = "CurrentPaneDomain" } },
+
   -- Fecha a aba atual
-  {key="x", mods="CTRL|SHIFT", action=wezterm.action{CloseCurrentTab={confirm=true}}},
-  
+  { key = "x",          mods = "CTRL|SHIFT",  action = wezterm.action { CloseCurrentTab = { confirm = true } } },
+
   -- Troca para a aba da esquerda
-  {key="LeftArrow", mods="CTRL|SHIFT", action=wezterm.action{ActivateTabRelative=-1}},
+  { key = "LeftArrow",  mods = "CTRL|SHIFT",  action = wezterm.action { ActivateTabRelative = -1 } },
 
   -- Troca para a aba da direita
-  {key="RightArrow", mods="CTRL|SHIFT", action=wezterm.action{ActivateTabRelative=1}},
+  { key = "RightArrow", mods = "CTRL|SHIFT",  action = wezterm.action { ActivateTabRelative = 1 } },
 
-  -- Dividir o painel horizontalmente
-  {key="h", mods="CTRL", action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
-
-  -- Dividir o painel verticalmente
-  {key="v", mods="CTRL", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
 
   -- Ajustar tamanho dos painéis (expandir/retrair)
-  {key="LeftArrow", mods="ALT|SHIFT", action=wezterm.action{AdjustPaneSize={"Left", 5}}},
-  {key="RightArrow", mods="ALT|SHIFT", action=wezterm.action{AdjustPaneSize={"Right", 5}}},
-  {key="UpArrow", mods="ALT|SHIFT", action=wezterm.action{AdjustPaneSize={"Up", 5}}},
-  {key="DownArrow", mods="ALT|SHIFT", action=wezterm.action{AdjustPaneSize={"Down", 5}}},
+  { key = "LeftArrow",  mods = "ALT|SHIFT",   action = wezterm.action { AdjustPaneSize = { "Left", 5 } } },
+  { key = "RightArrow", mods = "ALT|SHIFT",   action = wezterm.action { AdjustPaneSize = { "Right", 5 } } },
+  { key = "UpArrow",    mods = "ALT|SHIFT",   action = wezterm.action { AdjustPaneSize = { "Up", 5 } } },
+  { key = "DownArrow",  mods = "ALT|SHIFT",   action = wezterm.action { AdjustPaneSize = { "Down", 5 } } },
 
   -- Fechar o painel atual
-  {key="x", mods="SUPER", action=wezterm.action{CloseCurrentPane={confirm=true}}},
+  { key = "x",          mods = "SUPER",       action = wezterm.action { CloseCurrentPane = { confirm = true } } },
+
+
+  -- Dividir o painel horizontalmente
+  { key = "h",          mods = "CTRL|SUPER",  action = wezterm.action { SplitHorizontal = { domain = "CurrentPaneDomain" } } },
+
+  -- Dividir o painel verticalmente
+  { key = "v",          mods = "CTRL|SUPER",  action = wezterm.action { SplitVertical = { domain = "CurrentPaneDomain" } } },
 
   -- Movimentar entre os painéis
-  {key="h", mods="SUPER", action=wezterm.action{ActivatePaneDirection="Left"}},
-  {key="l", mods="SUPER", action=wezterm.action{ActivatePaneDirection="Right"}},
-  {key="k", mods="SUPER", action=wezterm.action{ActivatePaneDirection="Up"}},
-  {key="j", mods="SUPER", action=wezterm.action{ActivatePaneDirection="Down"}},
+  { key = "h",          mods = "SHIFT|SUPER", action = wezterm.action { ActivatePaneDirection = "Left" } },
+  { key = "l",          mods = "SHIFT|SUPER", action = wezterm.action { ActivatePaneDirection = "Right" } },
+  { key = "k",          mods = "SHIFT|SUPER", action = wezterm.action { ActivatePaneDirection = "Up" } },
+  { key = "j",          mods = "SHIFT|SUPER", action = wezterm.action { ActivatePaneDirection = "Down" } },
 }
